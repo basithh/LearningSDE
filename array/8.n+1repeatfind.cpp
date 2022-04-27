6ad920087a020b5faa4cc8a416b5e57ef60da9c9
basithh <basith8101@gmail.com>
2022-04-28 01:25:59 +0530

    v1 update

--- array/8.n+1repeatfind.cpp
+++ array/8.n+1repeatfind.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
-using 
+using namespace std;
+int main(){
+    int arr[6] = {1,2,3,4,4,5};
+    int n = 6;
+    int tempsum = 0;
+    for(auto num: arr){
+        tempsum += num;
+    }
+    int nsum = (n-1)*((n-1)+1)/2;
+    int ans = tempsum - nsum; 
+    cout<<ans;
+    
+}
